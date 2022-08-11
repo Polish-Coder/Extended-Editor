f214829dcde740274d85cbab8546a9bcbfd00552
Polygons Games Studio <polygonssoftware@gmail.com>
2022-08-11 12:45:29 +0200

    Added `FontStyle`, `Alignment`, `ContentOffset` to the style and added converting GUIStyle to ExtendedStyle when creating new one

--- Scripts/ExtendedStyle.cs
+++ Scripts/ExtendedStyle.cs
@@ -5,6 +5,20 @@ public class ExtendedStyle
     public Color32 FontColor = new();
     public Color32 FontHoverColor = new();
     public int FontSize = -1;
+    public FontStyle FontStyle = new();
+    public TextAnchor Alignment = new();
+    public Vector2 ContentOffset = new();
+
+    public ExtendedStyle() { }
+
+    public ExtendedStyle(GUIStyle other)
+    {
+        FontColor = other.normal.textColor;
+        FontHoverColor = other.hover.textColor;
+        FontSize = other.fontSize;
+        Alignment = other.alignment;
+        ContentOffset = other.contentOffset;
+    }
 
     public GUIStyle ConvertToGUIStyle()
     {
@@ -29,5 +43,8 @@ public class ExtendedStyle
         style.normal.textColor = !FontColor.Equals(new Color32()) ? FontColor : style.normal.textColor;
         style.hover.textColor = !FontHoverColor.Equals(new Color32()) ? FontHoverColor : style.normal.textColor;
         style.fontSize = FontSize != -1 ? FontSize : style.fontSize;
+        style.fontStyle = !FontStyle.Equals(new FontStyle()) ? FontStyle : style.fontStyle;
+        style.alignment = !Alignment.Equals(new TextAnchor()) ? Alignment : style.alignment;
+        style.contentOffset = !ContentOffset.Equals(Vector2.zero) ? ContentOffset : style.contentOffset;
     }
 }
